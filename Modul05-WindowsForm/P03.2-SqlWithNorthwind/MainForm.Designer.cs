11624a35a5222c7971159e23b9232ce3761d3cd0
Burak Can Zengin <brkcnzngn1934@gmail.com>
2023-12-22 14:29:53 +0300

    22-12-2023

---
+++ Modul05-WindowsForm/P03.2-SqlWithNorthwind/MainForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace P03_SqlWithNorthwind
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvProduct = new DataGridView();
+            cmbCategories = new ComboBox();
+            label1 = new Label();
+            btn_showAll = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvProduct).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvProduct
+            // 
+            dgvProduct.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvProduct.Dock = DockStyle.Bottom;
+            dgvProduct.Location = new Point(0, 204);
+            dgvProduct.Name = "dgvProduct";
+            dgvProduct.RowTemplate.Height = 25;
+            dgvProduct.Size = new Size(1301, 419);
+            dgvProduct.TabIndex = 0;
+            // 
+            // cmbCategories
+            // 
+            cmbCategories.FormattingEnabled = true;
+            cmbCategories.Items.AddRange(new object[] { "" });
+            cmbCategories.Location = new Point(12, 38);
+            cmbCategories.Name = "cmbCategories";
+            cmbCategories.Size = new Size(142, 23);
+            cmbCategories.TabIndex = 1;
+            cmbCategories.SelectedIndexChanged += cmbCategories_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Kategoriler";
+            // 
+            // btn_showAll
+            // 
+            btn_showAll.Location = new Point(178, 38);
+            btn_showAll.Name = "btn_showAll";
+            btn_showAll.Size = new Size(113, 23);
+            btn_showAll.TabIndex = 3;
+            btn_showAll.Text = "Tümünü Göster";
+            btn_showAll.UseVisualStyleBackColor = true;
+            btn_showAll.Click += btn_showAll_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(1301, 623);
+            Controls.Add(btn_showAll);
+            Controls.Add(label1);
+            Controls.Add(cmbCategories);
+            Controls.Add(dgvProduct);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Northwind";
+            Load += MainForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvProduct).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvProduct;
+        private ComboBox cmbCategories;
+        private Label label1;
+        private Button btn_showAll;
+    }
+}
---
+++ Modul05-WindowsForm/P03.2-SqlWithNorthwind/NorthwindDAL.cs
@@ -0,0 +1,147 @@
+﻿using P03_SqlWithNorthwind.Entities;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace P03_SqlWithNorthwind
+{
+    internal class NorthwindDAL
+    {
+        SqlConnection connection;
+
+        public NorthwindDAL()
+        {
+            connection = CreateConnection();
+        }
+
+        private SqlConnection CreateConnection()
+        {
+            string serverName = "DESKTOP-3O41JPC";
+            string dbName = "Northwind";
+            string connectionString = $"Server={serverName};Database={dbName};Trusted_Connection=true;TrustServerCertificate=true";
+            return new SqlConnection(connectionString);
+
+
+        }
+        private void OpenDataBase()
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+
+            }
+        }
+        private void CloseDataBase()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public DataTable GetAllProducts()
+        {
+            string queryString = @"
+            SELECT
+	           p.ProductID AS [ID],
+               p.ProductName AS [Ürün],
+               c.CategoryName AS [Kategori],
+               p.UnitPrice AS [Fiyat],
+               p.UnitsInStock AS [Stok] 
+             FROM Products p 
+	          JOIN Categories c 
+		       ON p.CategoryID=c.CategoryID";
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(queryString, connection);
+            DataTable dataTable = new DataTable();
+            OpenDataBase();
+            sqlDataAdapter.Fill(dataTable);
+            CloseDataBase();//aslında sql dataadapter sınıfından yaratılan nesnemiz oto olarak bağlantıyı kapatır.biz burada sadece mantığını anlayalım diye yazdık.
+            return dataTable;
+        }
+        public List<Category> GetAllCategories()
+        {
+            string queryString = @"
+               SELECT
+                 c.CategoryID,
+                 c.CategoryName,
+                 c.Description
+               FROM Categories c";
+            SqlCommand cmd = new SqlCommand(queryString, connection);
+            OpenDataBase();
+            SqlDataReader dataReader = cmd.ExecuteReader();
+            List<Category> categories = new List<Category>();
+            Category category;
+            categories.Add(new Category { Id = 0, Name = "Tümü" });
+            while (dataReader.Read() == true)
+            {
+                category = new Category()
+                {
+                    //Id = Convert.ToInt32(dataReader["CategoryID"])
+                    Id = (int)dataReader["CategoryID"],
+                    Name = dataReader["CategoryName"].ToString(),
+                    Description = (string)dataReader["Description"]
+                };
+                categories.Add(category);
+            }
+            CloseDataBase();
+            return categories;
+
+        }
+        //public DataTable GetAllProductsByCategoryId(int id)
+        //{
+        //    string queryString = @"
+        //        SELECT
+        //         p.ProductID AS [ID],
+        //         p.ProductName AS [Ürün],
+        //         c.CategoryName AS [Kategori],
+        //         p.UnitPrice AS [Fiyat],
+        //         p.UnitsInStock AS [Stok] 
+        //        FROM Products p 
+        //         JOIN Categories c 
+        //          ON p.CategoryID=c.CategoryID
+        //  WHERE c.CategoryID = "+id;
+        //    SqlDataAdapter dataAdapter=new SqlDataAdapter(queryString, connection);
+        //    DataTable table = new DataTable();
+        //    OpenDataBase();
+        //    dataAdapter.Fill(table);
+        //    CloseDataBase();
+        //    return table;
+        //}
+        public List<Product> GetAllProductsByCategoryId(int id)
+        {
+            string queryString = @$"
+                SELECT
+	                p.ProductID AS [ID],
+	                p.ProductName AS [Ürün],
+	                c.CategoryName AS [Kategori],
+	                p.UnitPrice AS [Fiyat],
+	                p.UnitsInStock AS [Stok] 
+                FROM Products p 
+	                JOIN Categories c 
+		                ON p.CategoryID=c.CategoryID
+		        WHERE c.CategoryID = @pId";
+
+            SqlCommand cmd = new SqlCommand(queryString, connection);
+            cmd.Parameters.AddWithValue("@pId", id);
+            OpenDataBase();
+            SqlDataReader dataReader = cmd.ExecuteReader();
+            List<Product> products = new List<Product>();
+            Product product;
+            while (dataReader.Read())
+            {
+                product = new Product()
+                {
+                    Id = (int)dataReader["Id"],
+                    Name = (string)dataReader["Ürün"],
+                    Category= (string)dataReader["Kategori"],
+                    Price = (decimal)dataReader["Fiyat"],
+                    Stock = (Int16)dataReader["Stok"]
+                   
+                };
+                products.Add(product);
+            }
+            CloseDataBase();
+            return products;
+
+        }
+    }
+}
